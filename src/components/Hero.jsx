292529f0015920101f9ee976f1503351dac986e3
akash-535 <akashbish9e@gmail.com>
2025-01-03 13:33:52 +0530

    table and start task done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         preis : "",
         details : "",
     }
-    const [form, setForm] = useState(formList);
+    const [value, setValue] = useState(formList);
     const [error, setError] = useState(false)
     const [upload, setUpload] = useState([]);
     const handleUpload = (event) => {
@@ -25,15 +25,15 @@ const Hero = () => {
         e.preventDefault();
         setError(true)
         if (
-          form.titel !== '' &&
-          form.datum !== '' &&
-          form.standort !== '' &&
-          form.preis !== '' &&
-          form.details !== ''
+          value.titel !== '' &&
+          value.datum !== '' &&
+          value.standort !== '' &&
+          value.preis !== '' &&
+          value.details !== ''
       ) {
-          setForm(formList)
+          setValue(formList)
           setError(false)
-          console.log(form)
+          console.log(value)
           Swal.fire({
               title: 'success',
               text: 'Do you want to continue',
@@ -46,8 +46,9 @@ const Hero = () => {
     <>
         <div className='py-[60px] flex flex-col justify-center items-center px-4'>
         <div className='text-center'>
-            <h1 className='text-4xl font-bold tracking-tight leading-[122%] text-oil'>Event einsenden</h1>
-            <p className='max-w-3xl mx-auto text-xl font-light leading-[150%] text-neutral-500 py-5'>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            <h1 className='text-4xl font-bold tracking-tight leading-[122%] text-oil max-lg:text-3xl max-md:text-2xl max-sm:text-xl'>Event einsenden</h1>
+            <p className='max-w-3xl mx-auto text-xl font-light leading-[150%] text-neutral-500 py-5 max-md:text-lg max-sm:text-base max-md:py-3 max-sm:pt-2'>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
         </div>
         <div className="flex gap-4 flex-wrap w-full max-w-[763px] bg-neutral-50 border border-dashed rounded-[30px] py-2 px-4 items-center">
       {upload.map((obj, i) => (
@@ -84,30 +85,30 @@ const Hero = () => {
   </span>
 </div>
     <form className='pt-5'>
-        <div className='flex gap-5 flex-col justify-center'>
-        <div className='flex gap-5 max-xl:flex-wrap px-5 md:justify-center max-lg:justify-center'>
+        <div className='flex gap-5 flex-col'>
+        <div className='flex gap-5 max-xl:flex-wrap px-5 max-md:justify-normal max-lg:justify-center'>
             <div className='w-6/12'>
-                <input type="text" placeholder='Titel' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setForm({...form,titel:e.target.value })} value={form.titel} />
-                <p className='text-red-500 pl-5 font-semibold'>{error && !form.titel ? "Field is requried" : ""}</p>
+                <input type="text" placeholder='Titel' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setValue({...value,titel:e.target.value })} value={value.titel} />
+                <p className='text-red-500 pl-5 font-semibold'>{error && !value.titel ? "Field is requried" : ""}</p>
             </div>
             <div className='w-6/12'>
-                <input type="text" placeholder='Datum' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setForm({...form,datum:e.target.value })} value={form.datum} />
-                <p className='text-red-500 pl-5 font-semibold'>{error && !form.datum ? "Field is requried" : ""}</p>
+                <input type="text" placeholder='Datum' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setValue({...value,datum:e.target.value })} value={value.datum} />
+                <p className='text-red-500 pl-5 font-semibold'>{error && !value.datum ? "Field is requried" : ""}</p>
             </div>
             </div>
-            <div className='flex gap-5 max-xl:flex-wrap px-5 md:justify-center max-lg:justify-center'>
+            <div className='flex gap-5 max-xl:flex-wrap px-5 max-md:justify-normal max-lg:justify-center'>
             <div className='w-6/12'>
-                <input type="text" placeholder='Standort' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setForm({...form,standort:e.target.value })} value={form.standort}/>
-                <p className='text-red-500 pl-5 font-semibold'>{error && !form.standort ? "Field is requried" : ""}</p>
+                <input type="text" placeholder='Standort' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setValue({...value,standort:e.target.value })} value={value.standort}/>
+                <p className='text-red-500 pl-5 font-semibold'>{error && !value.standort ? "Field is requried" : ""}</p>
             </div>
             <div className='w-6/12'>
-                <input type="text" placeholder='Preis' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setForm({...form,preis:e.target.value })} value={form.preis}/>
-                <p className='text-red-500 pl-5 font-semibold'>{error && !form.preis ? "Field is requried" : ""}</p>
+                <input type="text" placeholder='Preis' className="placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm w-[371px] max-md:max-w-[310px] border border-light-grey rounded-full py-[17px] px-4 !outline-none text-neutral-500 placeholder:tracking-tight" onChange={(e) => setValue({...value,preis:e.target.value })} value={value.preis}/>
+                <p className='text-red-500 pl-5 font-semibold'>{error && !value.preis ? "Field is requried" : ""}</p>
             </div>
         </div>
         <div>
-            <textarea placeholder='Details zur Veranstaltung' className='w-full max-w-[763px] !outline-none placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm border border-light-grey rounded-[30px] p-4 min-h-[132px] max-h-[132px] overflow-y-auto placeholder:pt-8 text-neutral-500 placeholder:tracking-tight' onChange={(e) => setForm({...form,details:e.target.value })} value={form.details}></textarea>
-            <p className='text-red-500 pl-5 font-semibold'>{error && !form.details ? "Field is requried" : ""}</p>
+            <textarea placeholder='Details zur Veranstaltung' className='w-full max-w-[763px] !outline-none placeholder:text-neutral-500 placeholder:font-bold placeholder:text-sm font-bold text-sm border border-light-grey rounded-[30px] p-4 min-h-[132px] max-h-[132px] overflow-y-auto placeholder:pt-8 text-neutral-500 placeholder:tracking-tight' onChange={(e) => setValue({...value,details:e.target.value })} value={value.details}></textarea>
+            <p className='text-red-500 pl-5 font-semibold'>{error && !value.details ? "Field is requried" : ""}</p>
         </div>
         <button onClick={formHandler} className='w-full max-w-[361px] bg-gradient-to-r from-blue-eyes to-ball-blue py-4 rounded-full text-white mx-auto font-bold text-base tracking-tight hover:bg-gradient-to-l duration-300 ease-linear'>Event einsenden</button>
         </div>
--- src/components/OtherTask.jsx
+++ src/components/OtherTask.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const OtherTask = () => {
+    // Add
+    const [numberOne, setNumberOne] = useState()
+    const [numberTwo, setNumberTwo] = useState()
+    const [add, setAdd] = useState(0)
+  
+    const addNumbers = () => {
+      setAdd(numberOne + numberTwo)
+    }
+    // Table
   const TABLE = []
   for (let i = 1; i <= 10; i++) {
     TABLE.push(
@@ -8,6 +17,16 @@ const OtherTask = () => {
         2 * {i} = {2 * i}
       </p>
     )
+  }
+//   Star Print 
+  let patternSize = 5;
+  const pattern = [];
+  for (let i = 0; i < patternSize; i++) {
+    let line = "";
+    for (let j = 0; j < patternSize; j++) {
+      line += "*";
+    }
+    pattern.push(<div key={i}>{line}</div>)
   }
   
   return (
@@ -17,8 +36,42 @@ const OtherTask = () => {
     <h2 className='text-center text-2xl font-bold pb-5'>Print table of 2</h2>
     <p className='text-xl'>{TABLE}</p>
   </div>
-  {/* Function */}
 
+  {/* Print Star Mathod */}
+<div className='text-center'>
+    <p className='text-center text-2xl font-bold py-5'>Star</p>
+    {pattern}</div>
+
+    {/* Function */}
+    <div className='flex items-center justify-center py-[60px] max-lg:py-10 flex-col'>
+      <div className='max-w-[1140px] px-4 mx-auto'>
+        <div className='flex flex-wrap items-center md:justify-center gap-4'>
+          <input
+            className='py-3 px-3 rounded-lg border border-black outline-none !placeholder-black font-semibold'
+            value={numberOne}
+            placeholder='0'
+            type='number'
+            onChange={(e) => setNumberOne(Number(e.target.value))}
+          />
+          <input
+            className='py-3 px-3 rounded-lg border border-black outline-none !placeholder-black font-semibold'
+            value={numberTwo}
+            placeholder='0'
+            type='number'
+            onChange={(e) => setNumberTwo(Number(e.target.value))}
+          />
+          <button
+            onClick={addNumbers}
+            className='py-3 px-3 border border-green bg-green-400 rounded-lg font-semibold'
+          >
+            Add
+          </button>
+        </div>
+        <p className='text-2xl font-bold text-green-500 max-md:text-xl text-center pt-5'>
+          Answer = {add}
+        </p>
+      </div>
+    </div>
   </>
   )
 }
